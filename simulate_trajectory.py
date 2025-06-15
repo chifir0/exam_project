0e55a6bde7f503c938e3737aeab5c8fa2cb4355d
chifir0 <84554781+chifir0@users.noreply.github.com>
2025-06-15 19:55:40 +0300

    Update simulate_trajectory.py

--- simulate_trajectory.py
+++ simulate_trajectory.py
@@ -1,11 +1,17 @@
 import numpy as np
 import plotly.graph_objects as go
+from tkinter import messagebox
 
 def calculate_trajectory(v0, theta, phi, wind_speed, wind_direction,
-                        mass, diameter, air_density, gravity): # TODO ПРИ ГРАНИЧНЫХ ЗНАЧЕНИЯХ УГЛОВ С ГРАФИКОМ ТВОРЯТСЯ НЕПРИЛИЧНЫЕ ВЕЩИ
+                        mass, diameter, air_density, gravity):
+
+    if theta < 0 or theta >= 90 or v0 <= 0 or v0 >= 3*10**8 or mass <= 0 or diameter <= 0 or air_density < 0 or gravity <= 0:
+        messagebox.showerror("Ошибка", "Проверьте правильность введенных данных")
+        return
+
     try:
         C_drag = 0.47
-        A = np.pi * (diameter/2)**2
+        S = np.pi * (diameter/2)**2
         
         theta_rad = np.radians(theta)
         phi_rad = np.radians(phi)
@@ -13,24 +19,27 @@ def calculate_trajectory(v0, theta, phi, wind_speed, wind_direction,
         
         vx = v0 * np.sin(theta_rad) * np.cos(phi_rad)
         vy = v0 * np.sin(theta_rad) * np.sin(phi_rad)
-        vz = v0 * np.cos(theta_rad)
-        
+        vz = v0 * np.cos(theta_rad)        
         wind_x = wind_speed * np.cos(wind_dir_rad)
         wind_y = wind_speed * np.sin(wind_dir_rad)
         
         x, y, z = [0], [0], [0]
         t = 0
-        dt = 0.01
+        if v0 <= 10000:
+            dt = 1/v0
+        else:
+            dt = 1/10000
         
         while t < 30 and z[-1] >= 0:
+            if x[-1] > 10**5 or y[-1] > 10**5 or z[-1] > 10**5:
+                break
             v_rel_x = vx - wind_x
             v_rel_y = vy - wind_y
             v_rel_z = vz
-            
             speed_rel = np.sqrt(v_rel_x**2 + v_rel_y**2 + v_rel_z**2)
             
             if speed_rel > 0:
-                drag = 0.5 * air_density * C_drag * A * speed_rel**2
+                drag = 0.5 * air_density * C_drag * S * speed_rel**2
                 ax = -drag * v_rel_x / (mass * speed_rel)
                 ay = -drag * v_rel_y / (mass * speed_rel)
                 az = -gravity - drag * v_rel_z / (mass * speed_rel)
@@ -41,15 +50,16 @@ def calculate_trajectory(v0, theta, phi, wind_speed, wind_direction,
             vy += ay * dt
             vz += az * dt
             
-            x.append(x[-1] + vx * dt)
-            y.append(y[-1] + vy * dt)
-            z.append(z[-1] + vz * dt)
+            x.append(round((x[-1] + vx * dt), 5))
+            y.append(round((y[-1] + vy * dt), 5))
+            z.append(round((z[-1] + vz * dt), 5))
             t += dt
         
         return x, y, z
         
     except Exception:
-        return None, None, None # ЧТО-ТО С ОШИБКАМИ ПРИДУМАТЬ
+        messagebox.showerror("Ошибка", f"Произошла ошибка при рассчете траектории: {Exception}")
+        return None, None, None
 
 def create_plot(x, y, z, params):
     if None in (x, y, z):
@@ -77,13 +87,17 @@ def create_plot(x, y, z, params):
         scene=dict(
             xaxis_title='X (м)',
             yaxis_title='Y (м)',
-            zaxis_title='Высота (м)',
+            zaxis_title='Z (м)',
             aspectmode='data'
         ),
-        title=f"Траектория снаряда (v₀={params['v0']} м/с, θ={params['theta']}°)"
+        title=f"Траектория снаряда (v₀={params['v0']} м/с, θ={params['theta']}°)\nМаксимальная высота H = {max(z)}"
     )
     
     return fig
+
+
+
+
 
 
 
