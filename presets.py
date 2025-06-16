637439c55d9c35253da7c5b23cb8249ff7fec2d2
chifir0 <84554781+chifir0@users.noreply.github.com>
2025-06-16 13:23:10 +0300

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,12 @@
+import os
+def save_preset(i, v0, theta, phi, wind_speed, wind_direction,
+                        mass, diameter, air_density, gravity, dt_param):
+    print('saving...')
+    file_path = os.path.join(os.path.dirname(__file__), "preset_data.txt")
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    new_preset = f'{v0} {theta} {phi} {wind_speed} {wind_direction} {mass} {diameter} {air_density} {gravity} {dt_param}'   
+    lines[i] = new_preset.rstrip('\n') + '\n'
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.writelines(lines)
